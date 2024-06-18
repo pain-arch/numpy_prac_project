9c9a733dad6160ef715b038dead28744cda869e9
pain-arch <himulol123@gmail.com>
2024-06-18 12:28:06 +0600

    feat: func for read the files

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import numpy as np
+
+
+def file_handling():
+  #open the file in read mode
+  lines = []
+  with open('company.txt','r') as file:
+    for line in file:
+      values = line.strip().split(',')
+      int_values = [int(val) for val in values]
+      lines.append(int_values)

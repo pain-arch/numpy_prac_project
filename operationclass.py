3b98ae32510c044983bf3ead574640687fc8f800
pain-arch <himulol123@gmail.com>
2024-06-19 13:31:14 +0600

    update operationclass.py

--- operationclass.py
+++ operationclass.py
@@ -4,3 +4,12 @@ class IntArray:
   def __init__(self, int_array):
     if not isinstance(int_array, np.ndarray) or int_array.dtype != 'int':
       raise ValueError("Input must be a numpy array of integers")
+      
+    self.int_array = int_array
+  
+  def display (self):
+    print(self.int_array)
+    
+  def salary(self):
+    array_shape = self.int_array.shape
+    money_for_product
